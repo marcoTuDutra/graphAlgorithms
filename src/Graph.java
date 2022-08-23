d5bc3e08b2cc027f4d3c57e5979b9aa7c6289cb0
marcoTuDutra <marco.dutra@aluno.ufop.edu.br>
2022-08-23 11:26:41 -0300

    Aula 22-08

--- src/Graph.java
+++ src/Graph.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Graph {
     private int countNodes;
     private int countEdges;
@@ -8,6 +14,29 @@ public class Graph {
         this.adjMatrix = new int[countNodes][countNodes];
     }
 
+    public Graph(String fileName) throws IOException {
+        File file = new File(fileName);
+        FileReader reader = new FileReader(file);
+        BufferedReader bufferedReader = new BufferedReader(reader);
+
+        // Read header
+        String[] line = bufferedReader.readLine().split(" ");
+        this.countNodes = (Integer.parseInt(line[0]));
+        int fileLines = (Integer.parseInt(line[1]));
+
+        // Create and fill adjMatrix with read edges
+        this.adjMatrix = new int[this.countNodes][this.countNodes];
+        for (int i = 0; i < fileLines; ++i) {
+            String[] edgeInfo = bufferedReader.readLine().split(" ");
+            int source = Integer.parseInt(edgeInfo[0]);
+            int sink = Integer.parseInt(edgeInfo[1]);
+            int weight = Integer.parseInt(edgeInfo[2]);
+            addEdge(source, sink, weight);
+        }
+        bufferedReader.close();
+        reader.close();
+    }
+
     public void addEdge(int source, int sink, int weight) {
         if (source < 0 || source > this.countNodes - 1 || sink < 0 || sink > this.countNodes - 1 || weight <= 0) {
             System.err.println("Invalid value for source, sink or weight");
@@ -15,6 +44,16 @@ public class Graph {
         }
         this.adjMatrix[source][sink] = weight;
         this.countEdges++;
+    }
+
+    public void addEdgeUnoriented(int u, int v, int w) {
+        if (u < 0 || u > this.countNodes - 1 || v < 0 || v > this.countNodes - 1 || w <= 0) {
+            System.err.println("Invalid value for source, sink or weight");
+            return;
+        }
+        this.adjMatrix[u][v] = w;
+        this.adjMatrix[v][u] = w;
+        this.countEdges+= 2;
     }
 
     public int degree(int node) {
@@ -66,24 +105,56 @@ public class Graph {
         return g2;
     }
 
-    public float density(){
-        return (this.countEdges)/(this.countNodes * (this.countNodes -1));
+    public float density() {
+        return (this.countEdges) / (this.countNodes * (this.countNodes - 1));
     }
 
-    public  boolean subGraf(Graph g2){
+    public boolean subGraf(Graph g2) {
         if (g2.getCountEdges() > this.countEdges || g2.getCountNodes() > this.countNodes) {
             return false;
-        }else{
+        } else {
             int[][] g2Mat = g2.getAdjMatrix();
             for (int i = 0; i < g2Mat.length; i++) {
                 for (int j = 0; j < g2Mat[i].length; j++) {
-                    if (g2Mat[i][j] != 0 && this.adjMatrix[i][j] == 0){
+                    if (g2Mat[i][j] != 0 && this.adjMatrix[i][j] == 0) {
                         return false;
                     }
                 }
             }
         }
         return true;
+    }
+
+    public ArrayList<Integer> bfs(int s) {
+
+        int[] desc = new int[this.countNodes];
+
+        ArrayList<Integer> Q = new ArrayList<>();
+        Q.add(s);
+
+        ArrayList<Integer> R = new ArrayList<>();
+        R.add(s);
+
+        desc[s] = 1;
+
+        while (Q.size() > 0) {
+            int u = Q.remove(0);
+
+            for (int v = 0; v < this.adjMatrix[u].length; v++) {
+                if (this.adjMatrix[u][v] != 0){
+                    if (desc[v] == 0){
+                        Q.add(v);
+                        R.add(v);
+                        desc[v] = 1;
+                    }
+                }
+            }
+        }
+        return R;
+    }
+    
+    public boolean connected(){
+        return  (this.bfs(0).size() == this.countNodes);
     }
 
     public int getCountNodes() {
--- src/Main.java
+++ src/Main.java
@@ -12,26 +12,42 @@ public class Main {
 //        System.out.println(g1.lowestDegree());
 //        System.out.println(g1.complement());
 
-        Graph g1 = new Graph(4);
-        Graph g2 = new Graph(3);
-        Graph g3 = new Graph(4);
+//        Graph g1 = new Graph(4);
+//        Graph g2 = new Graph(3);
+//        Graph g3 = new Graph(4);
+//
+//        g1.addEdge(0,1,1);
+//        g1.addEdge(0,3,1);
+//
+//        g2.addEdge(0,1,1);
+//
+//        g3.addEdge(3,1,1);
+//
+//        System.out.println(g1);
+//        System.out.println(" ");
+//        System.out.println(g2);
+//        System.out.println(" ");
+//        System.out.println(g3);
+//
+//        System.out.println(g1.subGraf(g2));
 
-        g1.addEdge(0,1,1);
-        g1.addEdge(0,3,1);
+        Graph g1 = new Graph(9);
 
-        g2.addEdge(0,1,1);
+        g1.addEdgeUnoriented(7,5,1);
+        g1.addEdgeUnoriented(7,1,1);
+        g1.addEdgeUnoriented(7,2,1);
 
-        g3.addEdge(3,1,1);
+        g1.addEdgeUnoriented(5,6,1);
 
-        System.out.println(g1);
-        System.out.println(" ");
-        System.out.println(g2);
-        System.out.println(" ");
-        System.out.println(g3);
+        g1.addEdgeUnoriented(1,0,1);
+        g1.addEdgeUnoriented(1,4,1);
 
-        System.out.println(g1.subGraf(g2));
+        g1.addEdgeUnoriented(2,3,1);
 
+        g1.addEdgeUnoriented(6,8,1);
 
+        System.out.println(g1.bfs(7));
+        System.out.println(g1.connected());
 
     }
 }
